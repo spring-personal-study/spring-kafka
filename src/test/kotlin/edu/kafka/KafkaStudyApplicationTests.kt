b869ac9e0e52d12f4b82e5c60b5abf7980e15b02
mclee <myoungcheol.lee@bluebirdcorp.com>
2025-04-17 10:06:53 +0900

    first commit

---
+++ src/test/kotlin/edu/kafka/KafkaStudyApplicationTests.kt
@@ -0,0 +1,13 @@
+package edu.kafka
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KafkaStudyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
